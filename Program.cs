9be027c99336bbef1cdd6b080bfee9b4b6ae979e
Giovanni Morais <giovannimo1@yahoo.com>
2025-06-10 15:30:45 -0300

    Validações no GET e SET

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Giovanni";
+p1.Idade = 20;
+p1.Apresentar();
