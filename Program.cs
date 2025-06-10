7344d29aea0500ce0c475065186d93a07e67db5b
Giovanni Morais <giovannimo1@yahoo.com>
2025-06-10 16:18:09 -0300

    Implementando Métodos

--- Program.cs
+++ Program.cs
@@ -3,5 +3,31 @@
 Pessoa p1 = new Pessoa();
 p1.Nome = "Giovanni";
 p1.Sobrenome = "Morais";
-p1.Idade = 20;
-p1.Apresentar();
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Eduardo";
+p2.Sobrenome = "Neves";
+
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Giovanni";
+// p1.Sobrenome = "Morais";
+// p1.Idade = 20;
+// p1.Apresentar();
